bd045137bcc2f01202ee7629b48f9c7fadbf5a65
nihui <18202504057@163.com>
2019-12-30 19:38:55 +0800

    增加SayHello类

---
+++ src/main/java/com/nihui/maven/tool/SayHello.java
@@ -0,0 +1,13 @@
+package com.nihui.maven.tool;
+
+/**
+ * @Classname SayHello
+ * @Description TODO
+ * @Date 2019/12/30 7:38 PM
+ * @Created by nihui
+ */
+public class SayHello {
+    public static String say(){
+        return "Hello World";
+    }
+}

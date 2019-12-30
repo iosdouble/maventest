ca6a9164201c471285cdd1ec6bfb6e344efd83ce
nihui <18202504057@163.com>
2019-12-30 20:00:34 +0800

    添加新模块测试Pom

---
+++ test-pom/src/main/java/com/nihui/testpom/TestPom.java
@@ -0,0 +1,16 @@
+package com.nihui.testpom;
+
+/**
+ * @Classname TestPom
+ * @Description TODO
+ * @Date 2019/12/30 7:51 PM
+ * @Created by nihui
+ */
+public class TestPom {
+    public void print(){
+        System.out.println("Test Pom");
+    }
+    public void hello(){
+        System.out.println("GHe");
+    }
+}

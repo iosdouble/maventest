01915b7129d4fc1496766a62e158b03ebaa58d28
nihui <18202504057@163.com>
2019-12-30 19:36:25 +0800

    创建获取时间类

---
+++ src/main/java/com/nihui/maven/tool/PrintDate.java
@@ -0,0 +1,15 @@
+package com.nihui.maven.tool;
+
+import java.util.Date;
+
+/**
+ * @Classname PrintDate
+ * @Description TODO
+ * @Date 2019/12/30 7:35 PM
+ * @Created by nihui
+ */
+public class PrintDate {
+    public static Date getDate(){
+        return new Date();
+    }
+}
